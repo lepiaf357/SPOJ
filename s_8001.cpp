1c7dbba32daebf0114803b37f6e4ec05e7cac9b5
git-nightbot.vestel.com.tr <nightbot@vestel.com.tr>
2016-06-20 17:34:24 +0200

    Modified programming p, and removed whitespace in some problems

--- s_8001.cpp
+++ s_8001.cpp
@@ -6,12 +6,12 @@ using namespace std;
 void matrix_mul_mod(long long A[2][2], long long B[2][2])
 {
     long long C[2][2];
-    
+
     C[0][0] = (A[0][0]*B[0][0] + A[0][1]*B[1][0]) % 1000000007;
     C[0][1] = (A[0][0]*B[0][1] + A[0][1]*B[1][1]) % 1000000007;
     C[1][0] = (A[1][0]*B[0][0] + A[1][1]*B[1][0]) % 1000000007;
     C[1][1] = (A[1][0]*B[0][1] + A[1][1]*B[1][1]) % 1000000007;
-    
+
     B[0][0]=C[0][0];
     B[0][1]=C[0][1];
     B[1][0]=C[1][0];
@@ -19,25 +19,25 @@ void matrix_mul_mod(long long A[2][2], long long B[2][2])
 }
 
 void fibonacci_matrix_pow(long long fibonacci_matrix_n[2][2], int n)
-{   
-    
+{
+
     long long fibonacci_tmp[2][2];
 
     fibonacci_tmp[0][0] = 1;
     fibonacci_tmp[0][1] = 1;
     fibonacci_tmp[1][0] = 1;
-    fibonacci_tmp[1][1] = 0;    
-    
+    fibonacci_tmp[1][1] = 0;
+
     fibonacci_matrix_n[0][0] = 1;
     fibonacci_matrix_n[0][1] = 0;
     fibonacci_matrix_n[1][0] = 0;
     fibonacci_matrix_n[1][1] = 1;
-        
-    while (n) 
+
+    while (n)
     {
         if (n & 1) // my boyfriend is a genius :) Love him so much!
             matrix_mul_mod(fibonacci_tmp, fibonacci_matrix_n);
-        
+
         matrix_mul_mod(fibonacci_tmp, fibonacci_tmp);
         n /= 2;
     }
@@ -46,44 +46,44 @@ void fibonacci_matrix_pow(long long fibonacci_matrix_n[2][2], int n)
 
 /*
  * return F(n)
- * 
+ *
  *  |1   1|^n    | F(n+1)  F(n)   |
- *  |     |   =  |                |   
+ *  |     |   =  |                |
  *  |1   0|      | F(n)    F(n-1) |
- * 
+ *
  * fibonacci_first^n [0][0] = F(n+1)
  * fibonacci_first^(n-1) [0][0] = F(n)
- * 
+ *
  */
-long long fibonacci(int n) 
+long long fibonacci(int n)
 {
     long long fibonacci_matrix_n[2][2];
-    
-    if (n == 0) 
+
+    if (n == 0)
         return 0;
 
     if (n == 1)
         return 1;
-    
+
     fibonacci_matrix_pow(fibonacci_matrix_n, n-1);
-    
+
     return fibonacci_matrix_n[0][0];
 }
 
 int main() {
-    
+
     int N, M, t;
     long long sumN_1, sumM, res;
-    
+
     scanf("%d", &t);
-    
-    while(t--) 
+
+    while(t--)
     {
         scanf("%d%d", &N, &M);
-        
+
         // Sum[i=1...n] F(i) = F(n+2) - 1
         // F(i) = fibonacci(i)
-        
+
         if (M == N)
         {
             res = fibonacci(M) % 1000000007;
@@ -94,13 +94,13 @@ int main() {
            sumN_1 = fibonacci(N + 1); // Sum 1...N-1        + 1
            res = (sumM - sumN_1) % 1000000007;
         }
-        
+
         // (7 - 2) mod 3 = 7 mod 3 - 2 mod 3 = 1 - 2 = -1
         // (7 - 2) mod 3 = 5 mod 3 = 2
         // -1 + 3 = 2
-        if (res < 0) 
+        if (res < 0)
             res += 1000000007;
-        
+
         printf("%lld\n", res);
     }
     return 0;
--- s_91.cpp
+++ s_91.cpp
@@ -3,23 +3,23 @@
 
 void two_squares(long long n)
 {
-    int  c = 0;   
+    int  c = 0;
     for(int i = 2; i*i <= n; i++)
     {
-        int j = 0;        
+        int j = 0;
         while(n % i == 0)
         {
             j++;
-            n=n/i;            
+            n=n/i;
         }
-        
+
         if(i % 4 == 3 && j % 2 != 0)
         {
             c = 1;
             break;
         }
     }
-    
+
     if (n % 4 == 3)
         c = 1;
 
@@ -34,20 +34,20 @@ int main()
 {
     int t;
     long long n;
-    
+
     scanf("%d", &t);
-    
+
     while (t--)
     {
         scanf("%lld", &n);
-        
+
         long long a = sqrt(n);
-        
+
         if (a*a == n)
            printf("Yes\n");
         else
            two_squares(n);
     }
-    
+
     return 0;
 }
