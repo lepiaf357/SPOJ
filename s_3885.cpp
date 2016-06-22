638982b0def47ce44cae5ad2d5d35c3e239a9152
git-nightbot.vestel.com.tr <nightbot@vestel.com.tr>
2016-06-22 14:20:01 +0200

    Coins added

--- s_3885.cpp
+++ s_3885.cpp
@@ -1,20 +1,46 @@
-#include <cstdio>
+// Players play until they reach a terminal pos
+// The player who can't make any move is declared a looser.
+// Rules:
+// 1. All terminal positions are loosing.
+// 2. If a player is able to move to a loosing position he is in a winning position.
+// 3. If a player is able to move to a wining position he is in a loosing position.
 
-int isWinning_r(int coin, int k, int l, int player)
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+#define W 1
+#define L 2
+
+int outcome[1000001];
+
+void outcomeSolve(int l, int k)
 {
-     if (coin == 0 && player == 1)
-          return 1;
-     if (coin == 0 && player == 0)
-          return 0;
-     // if (coin < 0 && player == 0)
-     //      return 0;
-     // if (coin < 0 && player == 1)
-     //      return 0;
-     int a, b, c;
-     a = isWinning_r(coin - 1, k, l, (player+1)%2);
-     b = isWinning_r(coin - k, k, l, (player+1)%2);
-     c = isWinning_r(coin - l, k, l, (player+1)%2);
-     return a || b || c;
+     int i;
+     outcome[0] = L;
+     outcome[1] = W;
+     for (i = 2; i < l; ++i)
+          if (outcome[i - 1] == L)
+               outcome[i] = W;
+          else
+               outcome[i] = L;
+     outcome[i] = W;
+     i++;
+     for (; i < k; ++i)
+          if (outcome[i - 1] == L || outcome[i - l] == L)
+               outcome[i] = W;
+          else
+               outcome[i] = L;
+     outcome[i] = W;
+     i ++;
+     for (; i <= 1000001; ++i)
+     {
+          if (outcome[i - 1] == L || outcome[i - l] == L || outcome[i - k] == L)//dati prednost poziciji pobede. optimalnost
+               outcome[i] = W;
+          else
+               outcome[i] = L;
+     }
 }
 
 int main()
@@ -22,13 +48,15 @@ int main()
      int n, k, l, coin;
 
      scanf("%d%d%d", &l, &k, &n);
+     outcomeSolve(l, k);
      while (n--)
      {
           scanf("%d", &coin);
-          if (isWinning_r(coin, k, l, 0))
+          if (outcome[coin] == W)
                printf("A");
           else
                printf("B");
      }
+     printf("\n");
      return 0;
 }
